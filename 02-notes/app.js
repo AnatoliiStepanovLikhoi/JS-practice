80b33b9a2809ca179e6111544b940799a7f50499
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-18 11:03:29 +0300

    add: deleting & toggle modes

--- 02-notes/app.js
+++ 02-notes/app.js
@@ -2,16 +2,25 @@ const inputElement = document.getElementById("title");
 const createBtn = document.getElementById("create");
 const listElement = document.getElementById("list");
 
-const notes = ["paint Volvo", "fix Getz"];
+const notes = [
+  { title: "paint Volvo", completed: false },
+  { title: "fix Getz", completed: true },
+];
 
 function render() {
-  //   for (i = 0; i < notes.length; i++) {
-  //     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]));
-  //   }
+  listElement.innerHTML = "";
 
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+  if (notes.length === 0) {
+    listElement.innerHTML = "<p>No elements</p>";
   }
+
+  for (i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i));
+  }
+
+  //   for (let note of notes) {
+  //     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+  //   }
 }
 
 render();
@@ -21,23 +30,45 @@ createBtn.onclick = function () {
     return;
   }
 
-  listElement.insertAdjacentHTML(
-    "beforeend",
-    getNoteTemplate(inputElement.value)
-  );
+  const newNote = {
+    title: inputElement.value,
+    completed: false,
+  };
+
+  notes.push(newNote);
+  render();
 
   inputElement.value = "";
   inputElement.focus();
 };
 
-function getNoteTemplate(title) {
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    const index = +event.target.dataset.index;
+    const type = event.target.dataset.type;
+
+    if (type === "toggle") {
+      notes[index].completed = !notes[index].completed;
+    } else if (type === "remove") {
+      notes.splice(index, 1);
+    }
+
+    render();
+  }
+};
+
+function getNoteTemplate(note, idx) {
   return `   <li
           class="list-group-item d-flex justify-content-between align-items-center"
         >
-          <span>${title}</span>
+          <span class="${
+            note.completed ? "text-decoration-line-through" : ""
+          }">${note.title}</span>
           <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            <span class="btn btn-small btn-${
+              note.completed ? "warning" : "success"
+            }" data-index="${idx}" data-type='toggle'>&check;</span>
+            <span class="btn btn-small btn-danger" data-type='remove' data-index="${idx}">&times;</span>
           </span>
         </li>`;
 }
